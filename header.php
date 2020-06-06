f32e3592f877ec80b7e507eb72b86643452ae32d
marhowlett <marimar_r2003@hotmail.com>
2020-06-06 13:23:29 -0500

    .

---
+++ header.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The Header template for our theme
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://kit.fontawesome.com/2845b75df5.js" crossorigin="anonymous"></script>
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<?php do_action( 'woodmart_after_body_open' ); ?>
+	
+	<div class="website-wrapper">
+
+		<?php if ( woodmart_needs_header() ): ?>
+
+			<!-- HEADER -->
+			<header <?php woodmart_get_header_classes(); // location: inc/functions.php ?>>
+
+				<?php 
+					whb_generate_header();
+				 ?>
+
+			</header><!--END MAIN HEADER-->
+			
+			<?php woodmart_page_top_part(); ?>
+
+		<?php endif ?>
